d3f886ea84f410f168baf7a986cf02578684e66f
Loren Gordon <lorengordon@users.noreply.github.com>
2021-03-17 10:37:20 -0700

    Pauses after removing the share accepter for the status to propagate

--- modules/share_accepter/main.tf
+++ modules/share_accepter/main.tf
@@ -1,3 +1,9 @@
 resource "aws_ram_resource_share_accepter" "this" {
   share_arn = var.resource_share_arn
+
+  # Leaving the share sometime takes a few seconds to propagate
+  provisioner "local-exec" {
+    command = "python -c 'import time; time.sleep(10)'"
+    when    = destroy
+  }
 }
